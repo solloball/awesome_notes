c96911b78ee65918632d83b7007c2fa84b5080d2
solloball <romanov.k48@gmail.com>
2024-06-03 14:10:06 +0700

    made config

--- cmd/notes/main.go
+++ cmd/notes/main.go
@@ -1,9 +1,17 @@
 package main
 
 
+import (
+    "fmt"
+
+    "github.com/solloball/aws_tg/internal/config"
+)
 
 func main() {
-    // TODO: config
+
+    conf := config.MustLoad()
+
+    fmt.Println(conf)
 
     //TODO: init logger
 

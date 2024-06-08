bc60d4ef850a143f81aa1e0dfb40ae65d1b59134
solloball <romanov.k48@gmail.com>
2024-06-08 21:15:28 +0700

    make simple form for making note and atach it to server

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import AddNote from './addNote';
 
-function App() {
+export class App extends React.Component {
+ constructor(props) {
+     super(props);
+ }
+ render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div>Pretty note</div>
       </header>
+      <aside>
+        <AddNote />
+      </aside>
     </div>
   );
+ }
 }
 
 export default App;
